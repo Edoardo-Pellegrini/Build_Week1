a45dd658cfe69e13ec772041413b0a3324a6676c
Edoardo-Pellegrini <pellegrini.ted@gmail.com>
2022-05-18 14:08:06 +0200

    Modifiche generali

--- js/script.js
+++ js/script.js
@@ -8,10 +8,10 @@
 
 function arrow() {
      
-    document.getElementById('prova1').style.transform = 'translateX(-10px)';
-   /*  document.getElementById('prova2').style.transform = 'translateX(-276px)';
+    document.getElementById('prova1').style.transform = 'translateX(-276px)';
+    document.getElementById('prova2').style.transform = 'translateX(-276px)';
     document.getElementById('prova3').style.transform = 'translateX(-276px)';
-    document.getElementById('prova4').style.transform = 'translateX(-276px)'; */
+    document.getElementById('prova4').style.transform = 'translateX(-276px)'; 
     console.log('ok')
 
     
